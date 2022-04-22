3057b08d3136882b4633b03e89d67d9d10920e9a
khanh2004 <100031281+khanh2004@users.noreply.github.com>
2022-04-22 22:52:06 +0700

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -12,3 +12,4 @@ lần này đã thành công chưa đây hả
 không biết nữa bạn ơi
 vậy là cũng ổn rồi thôi đi ngủ đi
 ừa t cũng mệt lắm rồi mai chắc là tao không đi tập thể dục được rồi mệt lắm hôm nay là cũng biết khá là nhiều thứ đó. Mai rồi ta học tiếp nhá hứa sẽ thay đổi bản thân nhiều hơn nữa
+ok bạn
