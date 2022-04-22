e35da79ee0148494ecebbf02d7fdb1bcaef03722
khanh2004 <duykhanhhoang@hotmail.com>
2022-04-22 22:55:17 +0700

    ghjg

--- Untitled-1.py
+++ Untitled-1.py
@@ -11,5 +11,6 @@ tao chỉ muốn được sử dụng thôi không có ý gì cả mệt quá đ
 lần này đã thành công chưa đây hả
 không biết nữa bạn ơi
 vậy là cũng ổn rồi thôi đi ngủ đi
-ừa t cũng mệt lắm rồi mai chắc là tao không đi tập thể dục được rồi mệt lắm hôm nay là cũng biết khá là nhiều thứ đó. Mai rồi ta học tiếp nhá hứa sẽ thay đổi bản thân nhiều hơn nữa
+ừa t cũng mệt lắm rồi mai chắc là tao không 
+đi tập thể dục được rồi mệt lắm hôm nay là cũng biết khá là nhiều thứ đó. Mai rồi ta học tiếp nhá hứa sẽ thay đổi bản thân nhiều hơn nữa
 ok bạn
