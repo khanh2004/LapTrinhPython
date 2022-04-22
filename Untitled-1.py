8ad7477da39e904aac2b18214b15ed55620169fa
khanh2004 <duykhanhhoang@hotmail.com>
2022-04-22 21:35:52 +0700

    biết dùng rồi ahihi

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,4 @@
 hoang duy khánh không được đẹp trai lắm đâu nha mọi người
 print("mày ngu vãi chưởng ra khánh ơi code cũng không biết code nữa thì làm ăn được gì bây giờ?")
 print("hình như là mình biết dùng github rồi hay sao ý mọi người ạ")
+print("biết dùng rồi à hay đó nha")
